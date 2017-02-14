9f554786ad85de592fea5536919f5fb2d1b9ed19
jiangzhifei <jiangzhifei@eskygo.com>
2017-02-14 15:32:15 +0800

    aes256

--- src/main/java/com/lakala/crossborder/client/util/AES256Crypto.java
+++ src/main/java/com/lakala/crossborder/client/util/AES256Crypto.java
@@ -5,7 +5,10 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
-import java.security.*;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
 
 /**
  * Created by jiang on 2017/2/8.
@@ -29,13 +32,14 @@ public class AES256Crypto {
      */
     public static byte[] encrypt(String content, String password) {
         try {
-            SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
-            secureRandom.setSeed(password.getBytes("utf-8"));
-            KeyGenerator kgen = KeyGenerator.getInstance("AES","BC");
-            kgen.init(256, secureRandom);
+//            SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+//            secureRandom.setSeed(password.getBytes("utf-8"));
+            KeyGenerator kgen = KeyGenerator.getInstance("AES", "BC");
+            kgen.init(256);
             SecretKey secretKey = kgen.generateKey();
-            byte[] enCodeFormat = secretKey.getEncoded();
-            SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
+//            byte[] enCodeFormat = secretKey.getEncoded();
+//            SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
+            SecretKeySpec key = new SecretKeySpec(password.getBytes("utf-8"), "AES");
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS7Padding", "BC");// 创建密码器
             byte[] byteContent = content.getBytes("GBK");
             cipher.init(Cipher.ENCRYPT_MODE, key);// 初始化
@@ -68,14 +72,14 @@ public class AES256Crypto {
      */
     public static byte[] decrypt(byte[] content, String password) {
         try {
-            SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
-            secureRandom.setSeed(password.getBytes("utf-8"));
+//            SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+//            secureRandom.setSeed(password.getBytes("utf-8"));
 
-            KeyGenerator kgen = KeyGenerator.getInstance("AES","BC");
-            kgen.init(256, secureRandom);
-            SecretKey secretKey = kgen.generateKey();
-            byte[] enCodeFormat = secretKey.getEncoded();
-            SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
+            KeyGenerator kgen = KeyGenerator.getInstance("AES", "BC");
+            kgen.init(256);
+//            SecretKey secretKey = kgen.generateKey();
+//            byte[] enCodeFormat = secretKey.getEncoded();
+            SecretKeySpec key = new SecretKeySpec(password.getBytes("utf-8"), "AES");
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS7Padding", "BC");// 创建密码器
             cipher.init(Cipher.DECRYPT_MODE, key);// 初始化
             byte[] result = cipher.doFinal(content);
