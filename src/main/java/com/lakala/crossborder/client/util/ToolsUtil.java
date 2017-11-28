75cb0f9c4a6d7a3bbfe6c8936aa9314c71472888
jiangzhifei <jiang.zhif@hotmail.com>
2017-11-28 15:43:07 +0800

    Update ToolsUtil.java
    update ToolsUtil

--- src/main/java/com/lakala/crossborder/client/util/ToolsUtil.java
+++ src/main/java/com/lakala/crossborder/client/util/ToolsUtil.java
@@ -17,7 +17,7 @@ public class ToolsUtil {
      * @param length 自定义长度
      * @return
      */
-    public static String getRandomString(int length) {
+     public static String getRandomString(int length) {
 //        String radStr = "w0eA*1l&Bht3oCz^2p-D=n45%Ev8F6+Gg$9jHxdI7Ev8F6+Gg$9jHxdI\7Jc8KkLrMyaNq@mOsP#QR/!STbUVuWXfYiZ~w0eA*1l&Bht3oCz^2Jc8KkLrMyaNq@mOsP#QR!STbUVuWXfYiZ~w0eA*1l&Bht3oCz^2p-D=n45%ÍÍp-D=n45%Ev8F6+Gg$9jHxdI7Jc8KkLrMyaNq@mOsP#QR!STbUVuWXfYiZ~";
 //        StringBuffer generateRandStr = new StringBuffer();
 //        Random rand = new Random();
@@ -26,14 +26,15 @@ public class ToolsUtil {
 //            generateRandStr.append(radStr.substring(randNum, randNum + 1));
 //        }
 //        return generateRandStr.toString();
-        String radStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        StringBuffer generateRandStr = new StringBuffer();
-        Random rand = new Random();
-        for (int i = 0; i < length; i++) {
-            int randNum = rand.nextInt(36);
-            generateRandStr.append(radStr.substring(randNum, randNum + 1));
-        }
-        return generateRandStr + "";
+//        String radStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+//        StringBuffer generateRandStr = new StringBuffer();
+//        Random rand = new Random();
+//        for (int i = 0; i < length; i++) {
+//            int randNum = rand.nextInt(36);
+//            generateRandStr.append(radStr.substring(randNum, randNum + 1));
+//        }
+        String code = RandomStringUtils.randomAlphanumeric(length);
+        return code;
     }
 
     /**
